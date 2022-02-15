75121d2054761cdfc07aef5c7483b3443da114a8
sid314 <81903825+sid314@users.noreply.github.com>
2022-02-15 20:02:20 +0530

    Initial commit

---
+++ src/main/java/calculator/app.java
@@ -0,0 +1,7 @@
+package calculator;
+public class app {
+    public static void main(String[] args) {
+        GUI calc = new GUI();
+        calc.dispose();
+    }
+}
---
+++ src/main/java/calculator/operatorButton.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import javax.swing.*;
+
+public class operatorButton extends JButton {
+    private final char operator;
+    operatorButton(char operator){
+        this.operator = operator;
+        setText(String.valueOf(operator));
+    }
+    public char getOperator(){
+        return operator;
+    }
+
+}
